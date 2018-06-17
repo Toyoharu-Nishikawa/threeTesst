23a0435fe583c6717f818ff4873fc4603c98168d
toyoharu nishikawa <toyohal@email.com>
2018-06-17 20:39:23 +0900

    add third.html

--- public/scripts/third.js
+++ public/scripts/third.js
@@ -43,47 +43,49 @@ const init=()=>{
   plane.receiveShadow = true
   scene.add(plane)
 
-  const vertices = [
-    new THREE.Vector3(1,3,1),
-    new THREE.Vector3(1,3,-1),
-    new THREE.Vector3(1,-1,1),
-    new THREE.Vector3(1,-1,-1),
-    new THREE.Vector3(-1,3,-1),
-    new THREE.Vector3(-1,3,1),
-    new THREE.Vector3(-1,-1,-1),
-    new THREE.Vector3(-1,-1,1),
-  ]
-  const faces = [
-    new THREE.Face3(0,2,1),
-    new THREE.Face3(2,3,1),
-    new THREE.Face3(4,6,5),
-    new THREE.Face3(6,7,5),
-    new THREE.Face3(4,5,1),
-    new THREE.Face3(5,0,1),
-    new THREE.Face3(7,6,2),
-    new THREE.Face3(6,3,2),
-    new THREE.Face3(5,7,0),
-    new THREE.Face3(7,2,0),
-    new THREE.Face3(1,3,4),
-    new THREE.Face3(3,6,4),
-  ]
+  const N = 8 
+  const d = 360/N
+  const x = [...Array(N)].map((v,i)=>5*Math.cos(i*d/180*Math.PI))
+  const y = [0,10] 
+  const z = [...Array(N)].map((v,i)=>5*Math.sin(i*d/180*Math.PI))
+
+
+  const points = y.map((y0,i)=>
+    x.map((x0,j)=>[x0, y0, z[j]])
+  )
+  console.log(points)
+  const pointLine = [].concat(...points)
+  const vertices= pointLine.map(v=>new THREE.Vector3(v[0], v[1],v[2]))
+
+  const faceList = [...Array(points.length-1)].map((p,i)=>{
+    const j0  =  N*i
+    const section = [...Array(N)].map((v,j)=>{
+      const face1 = new THREE.Face3(j0+j,j0+j+N, (j+1>N-1 ? j0+0 : j0+j+1))
+      const face2 = new THREE.Face3(j0+j,(j==0 ? j0+2*N-1:j0+j+N-1),j0+j+N)
+      const face = [face1, face2]
+      return face
+    })
+    return [].concat(...section)
+  })
+  const faces = [].concat(...faceList)
+
   const geom = new THREE.Geometry()
   geom.vertices = vertices
   geom.faces = faces
   geom.computeFaceNormals()
 
-  const cubeMaterial = new THREE.MeshLambertMaterial({
-    color: 0xffffff
+  const cylinderMaterial = new THREE.MeshLambertMaterial({
+    color: 0x7777ff, side:THREE.DoubleSide, wireframe: false 
   })
-  const cube= new THREE.Mesh(geom, cubeMaterial)
+  const cylinder =  new THREE.Mesh(geom, cylinderMaterial)
 
-  cube.position.x = -4
-  cube.position.y = 3 
-  cube.position.z = 0 
+  cylinder.position.x = 0 
+  cylinder.position.y = 0 
+  cylinder.position.z = 0 
 
-  cube.castShadow = true
+  cylinder.castShadow = true
 
-  scene.add(cube)
+  scene.add(cylinder)
 
   const spotLight = new THREE.SpotLight(0xffffff)
   spotLight.position.set(-20, 30, -5)
@@ -92,7 +94,7 @@ const init=()=>{
 
   scene.add(spotLight)
 
-  camera.position.x = -30
+  camera.position.x = -20
   camera.position.y = 40
   camera.position.z = 30
   camera.lookAt(scene.position)
@@ -104,22 +106,11 @@ const init=()=>{
   const gui = new dat.GUI()
   gui.add(controls, "rotationSpeed", 0, 0.5)
   gui.add(controls, "bouncingSpeed", 0, 0.5)
-/*
-  let step = 0
-  const renderScene = ()=>{
-    stats.update()
-    cube.rotation.x += controls.rotationSpeed
-    cube.rotation.y += controls.rotationSpeed
-    cube.rotation.z += controls.rotationSpeed
-    requestAnimationFrame(renderScene)
-    renderer.render(scene, camera)
-  }
-*/
+
   document.getElementById("WebGL-output").appendChild(renderer.domElement)
   window.addEventListener("resize",onResize,false)
 
   renderer.render(scene, camera)
- // renderScene() 
 }
 
 
